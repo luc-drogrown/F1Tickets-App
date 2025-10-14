9ba2c9e94d40f77d549821044815d53d9af51b0c
luc-drogrown <draganlucian18@gmail.com>
2025-10-14 22:33:04 +0300

    database and qrcode func added

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException {
+        TicketManager ticketManager = new TicketManager();
+        ticketManager.genBarcode("luci", "ceva");
+
+        String url = "jdbc:mysql://localhost:3306/javabase";
+        String username = "java";
+        String password = "password";
+
+        System.out.println("Connecting database ...");
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Database connected!");
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
+    }
+}
